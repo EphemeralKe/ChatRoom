e704558c424662394fac64f1f3b3379456231002
wang <820594484@qq.com>
2018-01-28 09:49:25 +0800

    add start(kb)

--- First.java
+++ First.java
@@ -2,5 +2,6 @@ public class First{
 	public static void main(String[] args){
 		System.out.println("My first java program, hello.");
 		FileChannel.open();
+		System.out.println("start(kb.cpp)");
 	}
 }
